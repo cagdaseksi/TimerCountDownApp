03d1a8e33b1a9eca8937700dc2de73cd909b57ba
cagdaseksi <cagdas.eksi@hotmail.com>
2019-01-03 08:20:55 +0300

    view controller updated.

--- TimerCountDownApp/ViewController.swift
+++ TimerCountDownApp/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var seconds = 30
+    var timer = Timer()
+    
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func slider(_ sender: UISlider) {
+        seconds = Int(sender.value)
+        lblTime.text = String(seconds)
+    }
+    
+    @IBAction func btnStart(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func btnStop(_ sender: Any) {
+        timer.invalidate()
+        seconds = 30
+        slider.setValue(30, animated: true)
+        lblTime.text = "30"
+    }
+    
+    @objc func counter()
+    {
+        seconds -= 1
+        lblTime.text = String(seconds)
+        
+        if (seconds == 0)
+        {
+            timer.invalidate()
+            //audioPlayer.play()
+        }
+    }
+    
 }
 
