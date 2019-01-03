2d549878ed3f86fa09ff6f569cb2899d2d0b2604
cagdaseksi <cagdas.eksi@hotmail.com>
2019-01-03 22:36:20 +0300

    code clear.

--- TimerCountDownApp/ViewController.swift
+++ TimerCountDownApp/ViewController.swift
@@ -1,7 +1,18 @@
+//
+//  ViewController.swift
+//  TimerCountDown
+//
+//  Created by MAC on 3/1/19.
+//  Copyright © 2019 cagdaseksi. All rights reserved.
+//
+
 import UIKit
+
 class ViewController: UIViewController {
+    
     var seconds = 30
     var timer = Timer()
+    
     @IBOutlet weak var lblTime: UILabel!
     @IBOutlet weak var slider: UISlider!
     
@@ -9,15 +20,18 @@ class ViewController: UIViewController {
         seconds = Int(sender.value)
         lblTime.text = String(seconds)
     }
+    
     @IBAction func btnStart(_ sender: Any) {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
     }
+    
     @IBAction func btnStop(_ sender: Any) {
         timer.invalidate()
         seconds = 30
         slider.setValue(30, animated: true)
         lblTime.text = "30"
     }
+    
     @objc func counter() {
         seconds -= 1
         //lblTime.text = String(seconds)
@@ -29,15 +43,16 @@ class ViewController: UIViewController {
         }
         slider.value = Float(seconds)
     }
+    
     func timeString(time:TimeInterval) -> String {
         let hours = Int(time) / 3600
         let minutes = Int(time) / 60 % 60
         let seconds = Int(time) % 60
         return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
     }
+    
     func Alert() {
         let alertController = UIAlertController(title: "Time's Up", message: "A standard alert.", preferredStyle: .alert)
-        
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
             print("cancel")
         }
@@ -47,10 +62,7 @@ class ViewController: UIViewController {
             print("OK")
         }
         alertController.addAction(OKAction)
-        
         self.present(alertController, animated: true)
-        
     }
     
 }
-
